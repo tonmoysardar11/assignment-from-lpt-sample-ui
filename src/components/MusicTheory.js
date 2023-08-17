7aec197cb19238b7b61ef02170ca492c25ce76df
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-08-17 17:12:45 +0530

    Music theory done

--- src/components/MusicTheory.js
+++ src/components/MusicTheory.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import TheroyCard from './TheroyCard'
+import {cardData} from '../sampleCardData'
 
 const MusicTheory = () => {
   return (
+    <>
+    <h1 className='text-3xl text-black my-4 mx-6'>Music Theory</h1>
     <div className='h-full w-full flex flex-wrap justify-start items-center p-4'>
-      <TheroyCard/>
-      <TheroyCard/>
-      <TheroyCard/>
-      <TheroyCard/>
+      {cardData.map((element,index)=>{
+        return <TheroyCard key={index} item={element}/>
+      })}
     </div>
+    </>
   )
 }
 
--- src/components/TheroyCard.js
+++ src/components/TheroyCard.js
@@ -1,6 +1,4 @@
 import React from "react";
-import card from "../media/card.webp";
-import profile from "../media/profile.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClockFour, faNewspaper, faStar } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,30 +10,30 @@ const TheroyCard = ({item}) => {
       </div>
 
       <img
-        src={card}
+        src={item.img}
         alt="sample card image"
         className="w-full h-3/5 rounded-2xl mx-auto"
       />
       <div className="absolute top-32 left-5 bg-white flex justify-start items-center rounded-full p-1 w-1/2">
         <span className="w-8 h-8 rounded-full overflow-hidden">
-          <img src={profile} alt="profile" />
+          <img src={item.profile} alt="profile" />
         </span>
-        <p className="mx-2 text-gray-600">Emily Lee</p>
+        <p className="mx-2 text-gray-600">{item.name}</p>
       </div>
 
       <div className="flex flex-col justify-start text-black">
-        <p className="m-2 ">Music 101</p>
+        <p className="m-2 ">{item.class}</p>
         <div className="mx-2 flex justify-between items-center">
           <span className="flex items-center text-sm">
             <FontAwesomeIcon icon={faClockFour} />
-            <p className="mx-2">2h 30m</p>
+            <p className="mx-2">{item.time}</p>
           </span>
           <span className="flex items-center text-sm">
             <FontAwesomeIcon icon={faStar} />
-            <p className="mx-2">4.8/5</p>
+            <p className="mx-2">{item.rating}/5</p>
           </span>
           <span className="flex justify-center items-center text-sm bg-gray-100 p-2 rounded-full w-16">
-            <p>$29</p>
+            <p>{item.price}</p>
           </span>
         </div>
       </div>
