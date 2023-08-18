04a61e7c0b8a9bd03b7d1c9e7fdad69bf9054eb9
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-08-18 10:22:55 +0530

    my learning done

--- src/components/MusicTheory.js
+++ src/components/MusicTheory.js
@@ -1,8 +1,49 @@
 import React from 'react'
 import TheroyCard from './TheroyCard'
-import {cardData} from '../sampleCardData'
+import profile from '../media/profile.webp'
+import card from '../media/card.webp'
+
 
 const MusicTheory = () => {
+  const cardData=[
+    {
+        name:'David',
+        profile: profile,
+        class: 'Music Theory',
+        time: '2h 30m',
+        rating: 4.8,
+        price: '$25',
+        img:card
+    },
+    {
+        name:'David',
+        profile: profile,
+        class: 'Music Theory',
+        time: '2h 30m',
+        rating: 4.8,
+        price: '$25',
+        img:card
+    },
+    {
+        name:'David',
+        profile: profile,
+        class: 'Music Theory',
+        time: '2h 30m',
+        rating: 4.8,
+        price: '$25',
+        img:card
+    },
+    {
+        name:'David',
+        profile: profile,
+        class: 'Music Theory',
+        time: '2h 30m',
+        rating: 4.8,
+        price: '$25',
+        img:card
+    }
+
+]
   return (
     <div className='max-h-full text-center md:text-left lg:mr-36'>
     <h1 className='text-3xl text-black mb-6 mx-6'>Music Theory</h1>
--- src/components/MyLearning.js
+++ src/components/MyLearning.js
@@ -4,32 +4,57 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const MyLearning = () => {
+  const data = [
+    {
+      img: card,
+      name: "Learn Guitar",
+      author: "John Smith",
+      completed: 69,
+    },
+    {
+      img: card,
+      name: "Music Theory 2.0",
+      author: "Emily Johnson",
+      completed: 27,
+    }
+  ];
+
   return (
     <div className="max-h-full text-center md:text-left lg:mr-36">
       <h1 className="text-3xl text-black mb-6 mx-6">My Learnings</h1>
       <div className="flex flex-wrap justify-start items-center px-4">
-        <div class="p-4 md:w-1/2 w-full">
-          <div class="relative flex border-2 rounded-3xl border-gray-200 border-opacity-90 p-2 w-full">
-            <img
-              src={card}
-              alt="sample card"
-              className="h-full w-36 rounded-2xl"
-            />
-            <div className="absolute top-3 right-3 bg-white w-6 h-6 rounded-full overflow-hidden flex justify-center items-center">
-              <FontAwesomeIcon
-                icon={faBookmark}
-                className="text-black text-sm"
-              />
+        {data.map((element, index) => {
+          return (
+            <div key={index} className="p-2 md:w-1/2 w-full">
+              <div className="relative flex border-2 rounded-3xl border-gray-200 border-opacity-90 p-2 w-full">
+                <img
+                  src={element.img}
+                  alt="sample card"
+                  className="h-full w-36 rounded-2xl"
+                />
+                <div className="absolute top-3 right-3 bg-white w-6 h-6 rounded-full overflow-hidden flex justify-center items-center">
+                  <FontAwesomeIcon
+                    icon={faBookmark}
+                    className="text-black text-sm"
+                  />
+                </div>
+                <div className="relative flex flex-col justify-between items-start ml-4 w-full">
+                  <div
+                  style={{width:`${100 - element.completed}%` }}
+                    className={`absolute top-12 right-3 h-1 bg-gray-500`}
+                  ></div>
+                  <span>
+                    <h2 className="text-gray-900 text-lg title-font font-semibold">
+                      {element.name}
+                    </h2>
+                    <p className="text-sm">{element.author}</p>
+                  </span>
+                  <p className="text-sm">{element.completed}% Completed</p>
+                </div>
+              </div>
             </div>
-            <div class="flex flex-col justify-start items-start ml-4 w-full">
-              <h2 class="text-gray-900 text-lg title-font font-semibold mb-3">
-                Learn Guitar
-              </h2>
-              <p class="leading-relaxed text-base">John Smith</p>
-              <div className="h-5 w-1/3 mr-0 bg-gray-500"></div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
